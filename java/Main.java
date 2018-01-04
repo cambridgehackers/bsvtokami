c9dc800df35db085b499fcc56e02c2298424f5bd
Jamey Hicks <jamey.hicks@gmail.com>
2018-01-04 17:37:39 -0500

    convert all the files passed as arguments to bsvtokami

--- java/Main.java
+++ java/Main.java
@@ -7,39 +7,53 @@ import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
 class Main {
-   public static void main(String[] args) {
-      try {
-	  File file = new File(args[0]);
-	  String[] components = file.getName().split("\\.");
-	  String pkgName = components[0];
-	  CharStream charStream = CharStreams.fromFileName(args[0]);
-         /*
-          * make Lexer
-          */
-	  Lexer lexer = new BSVLexer(charStream);
-         /*
-          * get a TokenStream on the Lexer
-          */
-         TokenStream tokenStream = new CommonTokenStream(lexer);
+    public static void main(String[] args) {
+        /*
+         * make Lexer
+         */
+        Lexer lexer = null;
 
-         /*
-          * make a Parser on the token stream
-          */
-         BSVParser parser = new BSVParser(tokenStream);
-         /*
-          * get the top node of the AST. This corresponds to the topmost rule of BSV.g4, "start"
-          */
-         ParserRuleContext packagedef = parser.packagedef();
-	 
-	 StaticAnalysis staticAnalysis = new StaticAnalysis();
-	 staticAnalysis.visit(packagedef);
+        /*
+         * make a Parser on the token stream
+         */
+        BSVParser parser = null;
 
-	 BSVToKami bsvToKami = new BSVToKami(pkgName, staticAnalysis);
-	 
-	 bsvToKami.visit(packagedef);
+        for (String filename: args) {
+            System.err.println("converting file " + filename);
+            try {
 
-      } catch (IOException e) {
-         e.printStackTrace();
-      }
-   }
+                File file = new File(filename);
+                String[] components = file.getName().split("\\.");
+                String pkgName = components[0];
+                CharStream charStream = CharStreams.fromFileName(filename);
+
+                if (lexer == null) {
+                    lexer = new BSVLexer(charStream);
+                    parser = new BSVParser(new CommonTokenStream(lexer));
+                } else {
+                    lexer.setInputStream(charStream);
+                    parser.setTokenStream(new CommonTokenStream(lexer));
+                }
+
+                /*
+                 * get the top node of the AST. This corresponds to the topmost rule of BSV.g4, "start"
+                 */
+                ParserRuleContext packagedef = parser.packagedef();
+
+                StaticAnalysis staticAnalysis = new StaticAnalysis();
+                staticAnalysis.visit(packagedef);
+
+                BSVToKami bsvToKami = new BSVToKami(pkgName, staticAnalysis);
+
+                bsvToKami.visit(packagedef);
+                System.out.println("");
+
+            } catch (IOException e) {
+                System.err.println("IOException " + e);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 }
