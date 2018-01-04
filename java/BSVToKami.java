b036bc8b7de518da459cb81ab60880169a01b943
Jamey Hicks <jamey.hicks@gmail.com>
2018-01-04 17:40:07 -0500

    working on bsv to kami conversion

--- java/BSVToKami.java
+++ java/BSVToKami.java
@@ -5,52 +5,52 @@ import java.io.*;
 
 class BSVExpressionConverter extends BSVBaseVisitor<Expression>
 {
-	@Override
-	public Expression visitIdentifier(BSVParser.IdentifierContext ctx) {
-	    return new VariableRead(ctx.getText());
-	}
-	@Override
-	public Expression visitCondExpr(BSVParser.CondExprContext ctx) {
-	    return new CondExpression(visit(ctx.pred),
-				      visit(ctx.expression(0)),
-				      visit(ctx.expression(1)));
-	}
-	@Override
-	public Expression visitSimpleCondExpr(BSVParser.SimpleCondExprContext ctx) {
-	    return new CondExpression(visit(ctx.binopexpr()),
-				      visit(ctx.expression(0)),
-				      visit(ctx.expression(1)));
-	}
-	@Override public Expression visitBinopexpr(BSVParser.BinopexprContext ctx) {
-	    if (ctx.left == null) {
-		return visit(ctx.unopexpr());
-	    } else {
-		Expression left = visit(ctx.left);
-		String op = ctx.op.getText();
-		if (ctx.right != null) {
-		    return new OperatorExpression(op, left, visit(ctx.right));
-		} else {
-		    return new OperatorExpression(op, left);
-		}
-	    }
-	}
-	@Override public Expression visitUnopexpr(BSVParser.UnopexprContext ctx) {
-	    if (ctx.op != null) {
-		String op = ctx.op.getText();
-		if (ctx.exprprimary() != null) {
-		    return new OperatorExpression(op, visit(ctx.exprprimary()));
-		} else {
-		    return new OperatorExpression(op, visit(ctx.unopexpr()));
-		}
-	    } else {
-		return visit(ctx.exprprimary());
-	    }
-	}
+        @Override
+        public Expression visitIdentifier(BSVParser.IdentifierContext ctx) {
+            return new VariableRead(ctx.getText());
+        }
+        @Override
+        public Expression visitCondExpr(BSVParser.CondExprContext ctx) {
+            return new CondExpression(visit(ctx.pred),
+                                      visit(ctx.expression(0)),
+                                      visit(ctx.expression(1)));
+        }
+        @Override
+        public Expression visitSimpleCondExpr(BSVParser.SimpleCondExprContext ctx) {
+            return new CondExpression(visit(ctx.binopexpr()),
+                                      visit(ctx.expression(0)),
+                                      visit(ctx.expression(1)));
+        }
+        @Override public Expression visitBinopexpr(BSVParser.BinopexprContext ctx) {
+            if (ctx.left == null) {
+                return visit(ctx.unopexpr());
+            } else {
+                Expression left = visit(ctx.left);
+                String op = ctx.op.getText();
+                if (ctx.right != null) {
+                    return new OperatorExpression(op, left, visit(ctx.right));
+                } else {
+                    return new OperatorExpression(op, left);
+                }
+            }
+        }
+        @Override public Expression visitUnopexpr(BSVParser.UnopexprContext ctx) {
+            if (ctx.op != null) {
+                String op = ctx.op.getText();
+                if (ctx.exprprimary() != null) {
+                    return new OperatorExpression(op, visit(ctx.exprprimary()));
+                } else {
+                    return new OperatorExpression(op, visit(ctx.unopexpr()));
+                }
+            } else {
+                return visit(ctx.exprprimary());
+            }
+        }
     @Override public Expression visitIntliteral(BSVParser.IntliteralContext ctx) {
-	return new IntExpression(ctx.IntLiteral().getText());
+        return new IntExpression(ctx.IntLiteral().getText());
     }
     @Override public Expression visitRealliteral(BSVParser.RealliteralContext ctx) {
-	return new RealExpression(ctx.RealLiteral().getText());
+        return new RealExpression(ctx.RealLiteral().getText());
     }
 }
 
@@ -63,175 +63,184 @@ public class BSVToKami extends BSVBaseVisitor<Void>
     private ModuleDef moduleDef;
     private BSVExpressionConverter expressionConverter;
     BSVToKami(String pkgName, StaticAnalysis scopes) {
-	this.scopes = scopes;
-	this.pkgName = pkgName;
-	expressionConverter = new BSVExpressionConverter();
-	pkg = new Package(pkgName);
+        this.scopes = scopes;
+        this.pkgName = pkgName;
+        expressionConverter = new BSVExpressionConverter();
+        pkg = new Package(pkgName);
     }
 
     @Override
     public Void visitPackagedef(BSVParser.PackagedefContext ctx) {
-	System.err.println("Package " + pkgName);
-	if (ctx.packagedecl() != null) {
-	    if (!pkgName.equals(ctx.packagedecl().pkgname)) {
-		System.err.println("Expected " + pkgName + " found " + ctx.packagedecl().pkgname);
-	    }
-	}
-	return visitChildren(ctx);
+        System.err.println("Package " + pkgName);
+        if (ctx.packagedecl() != null) {
+            if (!pkgName.equals(ctx.packagedecl().pkgname)) {
+                System.err.println("Expected " + pkgName + " found " + ctx.packagedecl().pkgname);
+            }
+        }
+        return visitChildren(ctx);
+    }
+
+    @Override public Void visitTypeclassinstance(BSVParser.TypeclassinstanceContext ctx) {
+        scope = scopes.getScope(ctx);
+        visitChildren(ctx);
+        scope = scope.parent;
+        return null;
     }
 
     @Override public Void visitModuledef(BSVParser.ModuledefContext ctx) {
-	scope = scopes.getScope(ctx);
-	String moduleName = ctx.moduleproto().modulename.getText();
-	moduleDef = new ModuleDef(moduleName);
-	pkg.addStatement(moduleDef);
-	System.out.println("Definition " + moduleName + " := MODULE {" + "\n");
-	visitChildren(ctx);
-	System.out.println("}. (*" + ctx.moduleproto().modulename.getText() + " *)" + "\n");
-	scope = scope.parent;
-	moduleDef = null;
-	return null;
+        scope = scopes.getScope(ctx);
+        String moduleName = ctx.moduleproto().modulename.getText();
+        moduleDef = new ModuleDef(moduleName);
+        pkg.addStatement(moduleDef);
+        System.err.println("module " + moduleName);
+        System.out.println("Definition " + moduleName + " := MODULE {" + "\n");
+        visitChildren(ctx);
+        System.out.println("}. (*" + ctx.moduleproto().modulename.getText() + " *)" + "\n");
+        scope = scope.parent;
+        moduleDef = null;
+        System.err.println("endmodule : " + moduleName);
+        return null;
     }
 
     BSVParser.CallexprContext getCall(ParserRuleContext ctx) {
-	//FIXME: use type info
-	return null;
+        //FIXME: use type info
+        return null;
     }
 
     @Override public Void visitVarBinding(BSVParser.VarBindingContext ctx) {
-	String typeName = ctx.t.getText();
-	for (BSVParser.VarinitContext varinit: ctx.varinit()) {
-	    String varName = varinit.var.getText();
-	    System.out.println("    Variable " + varName + ": " + typeName + ".");
-	    BSVParser.ExpressionContext rhs = varinit.rhs;
-	    Expression expr = expressionConverter.visit(rhs);
-	}	
-	return null;
+        String typeName = ctx.t.getText();
+        for (BSVParser.VarinitContext varinit: ctx.varinit()) {
+            String varName = varinit.var.getText();
+            System.out.println("    Variable " + varName + ": " + typeName + ".");
+            BSVParser.ExpressionContext rhs = varinit.rhs;
+            if (rhs != null)
+                expressionConverter.visit(rhs);
+        }
+        return null;
     }
     @Override public Void visitLetBinding(BSVParser.LetBindingContext ctx) {
-	BSVParser.ExpressionContext rhs = ctx.rhs;
-	Expression expr = expressionConverter.visit(rhs);
-	for (BSVParser.LowerCaseIdentifierContext ident: ctx.lowerCaseIdentifier()) {
-	    String varName = ident.getText();
-	    System.out.println("    Variable " + varName + ": " + "TBDLetType" + ".");
-	}
-	return null ;
+        BSVParser.ExpressionContext rhs = ctx.rhs;
+        Expression expr = expressionConverter.visit(rhs);
+        for (BSVParser.LowerCaseIdentifierContext ident: ctx.lowerCaseIdentifier()) {
+            String varName = ident.getText();
+            System.out.println("    Variable " + varName + ": " + "TBDLetType" + ".");
+        }
+        return null ;
     }
     @Override public Void visitActionBinding(BSVParser.ActionBindingContext ctx) {
-	String typeName = ctx.t.getText();
-	String varName = ctx.var.getText();
-	BSVParser.ExpressionContext rhs = ctx.rhs;
-	Expression expr = expressionConverter.visit(rhs);
-	if (typeName.startsWith("Reg")) {
-	    System.out.print("    Register \"" + varName + "\" : " + "TBDType"
-			     + " <- ");
-	    
-	    BSVParser.CallexprContext call = getCall(ctx.rhs);
-	    if (call != null && call.fcn != null && call.fcn.getText().equals("mkReg")) {
-		System.out.print(call.expression().get(0).getText());
-	    } else {
-		visit(ctx.rhs);
-	    }
+        String typeName = ctx.t.getText();
+        String varName = ctx.var.getText();
+        BSVParser.ExpressionContext rhs = ctx.rhs;
+        Expression expr = expressionConverter.visit(rhs);
+        if (typeName.startsWith("Reg")) {
+            System.out.print("    Register \"" + varName + "\" : " + "TBDType"
+                             + " <- ");
 
-	    System.out.println("");
-	}
-	return null;
+            BSVParser.CallexprContext call = getCall(ctx.rhs);
+            if (call != null && call.fcn != null && call.fcn.getText().equals("mkReg")) {
+                System.out.print(call.expression().get(0).getText());
+            } else {
+                visit(ctx.rhs);
+            }
+
+            System.out.println("");
+        }
+        return null;
     }
 
     @Override public Void visitRuledef(BSVParser.RuledefContext ruledef) {
-	scope = scopes.getScope(ruledef);
-	String ruleName = ruledef.rulename.getText();
-	RuleDef ruleDef = new RuleDef(ruleName);
-	BSVParser.RulecondContext rulecond = ruledef.rulecond();
-	moduleDef.addRule(ruleDef);
+        scope = scopes.getScope(ruledef);
+        String ruleName = ruledef.rulename.getText();
+        RuleDef ruleDef = new RuleDef(ruleName);
+        BSVParser.RulecondContext rulecond = ruledef.rulecond();
+        moduleDef.addRule(ruleDef);
 
-	System.out.println("    with Rule \"" + ruleName + "\" :=" + "\n");
-	if (rulecond != null) {
-	    System.out.print("        Assert(");
-	    visit(rulecond);
-	    System.out.println(")");
-	}
-	visit(ruledef.rulebody());
-	System.out.println("        Retv (* rule " + ruledef.rulename.getText() + " *)" + "\n");
-	scope = scope.parent;
-	return null;
+        System.out.println("    with Rule \"" + ruleName + "\" :=" + "\n");
+        if (rulecond != null) {
+            System.out.print("        Assert(");
+            visit(rulecond);
+            System.out.println(")");
+        }
+        visit(ruledef.rulebody());
+        System.out.println("        Retv (* rule " + ruledef.rulename.getText() + " *)" + "\n");
+        scope = scope.parent;
+        return null;
     }
 
     @Override public Void visitRegwrite(BSVParser.RegwriteContext regwrite) {
-	System.out.print("        WriteReg ");
-	visitChildren(regwrite.lhs);
-	System.out.print(" <- ");
-	visitChildren(regwrite.rhs);
-	System.out.println(";" + "\n");
-	return null;
+        System.out.print("        WriteReg ");
+        visitChildren(regwrite.lhs);
+        System.out.print(" <- ");
+        visitChildren(regwrite.rhs);
+        System.out.println(";" + "\n");
+        return null;
     }
 
     @Override
     public Void visitPattern(BSVParser.PatternContext ctx) {
-	//FIXME
-	System.out.print("$" + ctx.getText());
-	return null;
+        //FIXME
+        System.out.print("$" + ctx.getText());
+        return null;
     }
 
     @Override
     public Void visitCondpredicate(BSVParser.CondpredicateContext ctx) {
-	visit(ctx.expression());
-	// if (ctx.pattern() != null) {
-	//     System.out.print(" matches ");
-	//     visit(ctx.pattern());
-	// }
-	if (ctx.condpredicate() != null) {
-	    System.out.print(" && ");
-	    visit(ctx.condpredicate());
-	}
-	return null;
+        visit(ctx.expression());
+        // if (ctx.pattern() != null) {
+        //     System.out.print(" matches ");
+        //     visit(ctx.pattern());
+        // }
+        if (ctx.condpredicate() != null) {
+            System.out.print(" && ");
+            visit(ctx.condpredicate());
+        }
+        return null;
     }
     @Override public Void visitBinopexpr(BSVParser.BinopexprContext expr) {
-	if (expr.right != null) {
-	    System.out.print("(");
-	    if (expr.left != null)
-		visit(expr.left);
-	    if (expr.op != null) {
-		System.out.print(" " + expr.op.getText() + " ");
-	    }
+        if (expr.right != null) {
+            System.out.print("(");
+            if (expr.left != null)
+                visit(expr.left);
+            if (expr.op != null) {
+                System.out.print(" " + expr.op.getText() + " ");
+            }
 
-	    visit(expr.right);
-	    System.out.print(")");
-	} else {
-	    visitChildren(expr);
-	}
-	return null;
+            visit(expr.right);
+            System.out.print(")");
+        } else {
+            visitChildren(expr);
+        }
+        return null;
     }
     @Override public Void visitUnopexpr(BSVParser.UnopexprContext ctx) {
-	if (ctx.op != null) {
-	    System.out.print(ctx.op.getText());
-	}
-	return visitChildren(ctx);
+        if (ctx.op != null) {
+            System.out.print(ctx.op.getText());
+        }
+        return visitChildren(ctx);
     }
     @Override public Void visitIntliteral(BSVParser.IntliteralContext ctx) {
-	System.out.print("$" + ctx.IntLiteral().getText());
-	return null;
+        System.out.print("$" + ctx.IntLiteral().getText());
+        return null;
     }
     @Override public Void visitRealliteral(BSVParser.RealliteralContext ctx) {
-	System.out.print("$" + ctx.RealLiteral().getText());
-	return null;
+        System.out.print("$" + ctx.RealLiteral().getText());
+        return null;
     }
     @Override public Void visitVarexpr(BSVParser.VarexprContext ctx) {
-	if (ctx.anyidentifier() != null) {
-	    String varName = ctx.anyidentifier().getText();
-	    System.err.println("var " + varName);
-	    if (scope.containsKey(varName)) {
-		SymbolTableEntry entry = scope.lookup(varName);
-		System.err.println("found binding " + varName + " " + entry.type);
-		if (entry.type.startsWith("Reg"))
-		    System.out.print("(ReadReg " + varName + ")");
-		else
-		    System.out.print(varName);
-	    } else {
-		System.out.print(varName);
-	    }
-	}
-	return null;
+        if (ctx.anyidentifier() != null) {
+            String varName = ctx.anyidentifier().getText();
+            System.err.println("var " + varName + " scope " + scope);
+            if (scope.containsKey(varName)) {
+                SymbolTableEntry entry = scope.lookup(varName);
+                System.err.println("found binding " + varName + " " + entry.type);
+                if (entry.type.startsWith("Reg"))
+                    System.out.print("(ReadReg " + varName + ")");
+                else
+                    System.out.print(varName);
+            } else {
+                System.out.print(varName);
+            }
+        }
+        return null;
     }
-
 }
--- java/StaticAnalysis.java
+++ java/StaticAnalysis.java
@@ -8,126 +8,152 @@ public class StaticAnalysis extends BSVBaseVisitor<Void>
 {
     private SymbolTable symbolTable;
     private HashMap<ParserRuleContext, SymbolTable> scopes;
-    
+
     StaticAnalysis() {
-	// global table
-	symbolTable = new SymbolTable(null);
-	scopes = new HashMap<ParserRuleContext, SymbolTable>();
+        // global table
+        symbolTable = new SymbolTable(null);
+        scopes = new HashMap<ParserRuleContext, SymbolTable>();
     }
 
     SymbolTable getScope(ParserRuleContext def) {
-	if (scopes.containsKey(def)) {
-	    return (SymbolTable)scopes.get(def);
-	} else {
-	    return null;
-	}
-    }	
+        if (scopes.containsKey(def)) {
+            return (SymbolTable)scopes.get(def);
+        } else {
+            return null;
+        }
+    }
 
     @Override
     public Void visitInterfacedecl(BSVParser.InterfacedeclContext ctx) {
-	String interfaceName = ctx.typedeftype().typeide().getText();
-	symbolTable.bind(interfaceName,
-			 new SymbolTableEntry(interfaceName, "interface"));
-	return null;
+        String interfaceName = ctx.typedeftype().typeide().getText();
+        symbolTable.bind(interfaceName,
+                         new SymbolTableEntry(interfaceName, "interface"));
+
+        symbolTable = new SymbolTable(symbolTable);
+        scopes.put(ctx, symbolTable);
+
+        visitChildren(ctx);
+
+        return null;
+    }
+
+    @Override public Void visitTypedefenum(BSVParser.TypedefenumContext ctx) {
+        String typedefname = ctx.upperCaseIdentifier().getText();
+        symbolTable.bind(typedefname,
+                         new SymbolTableEntry(typedefname, "struct"));
+        for (BSVParser.TypedefenumelementContext elt: ctx.typedefenumelement()) {
+            String tagname = elt.upperCaseIdentifier().getText();
+            symbolTable.bind(tagname, new SymbolTableEntry(tagname, typedefname));
+        }
+        return null;
+    }
+
+    @Override public Void visitTypedefstruct(BSVParser.TypedefstructContext ctx) {
+        String typedefname = ctx.typedeftype().getText();
+        return null;
+    }
+
+    @Override public Void visitTypedeftaggedunion(BSVParser.TypedeftaggedunionContext ctx) {
+        String typedefname = ctx.typedeftype().typeide().getText();
+        symbolTable.bind(typedefname,
+                         new SymbolTableEntry(typedefname, "tagged union"));
+        for (BSVParser.UnionmemberContext member: ctx.unionmember()) {
+            BSVParser.UpperCaseIdentifierContext id = member.upperCaseIdentifier();
+            if (id != null) {
+                String idname = id.getText();
+                symbolTable.bind(idname,
+                                 new SymbolTableEntry(idname, typedefname));
+            } else if (member.substruct() != null) {
+            } else if (member.subunion() != null) {
+            }
+        }
+        return null;
+    }
+
+    @Override public Void visitTypeclassdecl(BSVParser.TypeclassdeclContext ctx) {
+        symbolTable = new SymbolTable(symbolTable);
+        scopes.put(ctx, symbolTable);
+
+        visitChildren(ctx);
+
+        symbolTable = symbolTable.parent;
+        return null;
+    }
+
+    @Override public Void visitTypeclassinstance(BSVParser.TypeclassinstanceContext ctx) {
+        symbolTable = new SymbolTable(symbolTable);
+        scopes.put(ctx, symbolTable);
+
+        visitChildren(ctx);
+
+        symbolTable = symbolTable.parent;
+        return null;
     }
 
     @Override
     public Void visitTypedefsynonym(BSVParser.TypedefsynonymContext ctx) {
-	String typedefname = ctx.typedeftype().typeide().getText();
-	String type;
-	if (ctx.type() != null)
-	    type = ctx.type().getText();
-	else
-	    type = ctx.functionproto().getText();
-	symbolTable.bind(typedefname,
-			 new SymbolTableEntry(typedefname, type));
-	return null;
-    }
-
-    @Override public Void visitTypedefenum(BSVParser.TypedefenumContext ctx) {
-	String typedefname = ctx.upperCaseIdentifier().getText();
-	symbolTable.bind(typedefname,
-			 new SymbolTableEntry(typedefname, "struct"));
-	for (BSVParser.TypedefenumelementContext elt: ctx.typedefenumelement()) {
-	    String tagname = elt.upperCaseIdentifier().getText();
-	    symbolTable.bind(tagname, new SymbolTableEntry(tagname, typedefname));
-	}
-	return null;
-    }
-
-    @Override public Void visitTypedefstruct(BSVParser.TypedefstructContext ctx) {
-	String typedefname = ctx.typedeftype().getText();
-	return null;
-    }
-
-    @Override public Void visitTypedeftaggedunion(BSVParser.TypedeftaggedunionContext ctx) {
-	String typedefname = ctx.typedeftype().typeide().getText();
-	symbolTable.bind(typedefname,
-			 new SymbolTableEntry(typedefname, "tagged union"));
-	for (BSVParser.UnionmemberContext member: ctx.unionmember()) {
-	    BSVParser.UpperCaseIdentifierContext id = member.upperCaseIdentifier();
-	    if (id != null) {
-		String idname = id.getText();
-		symbolTable.bind(idname,
-				 new SymbolTableEntry(idname, typedefname));
-	    } else if (member.substruct() != null) {
-	    } else if (member.subunion() != null) {
-	    }
-	}
-	return null;
+        String typedefname = ctx.typedeftype().typeide().getText();
+        String type;
+        if (ctx.type() != null)
+            type = ctx.type().getText();
+        else
+            type = ctx.functionproto().getText();
+        symbolTable.bind(typedefname,
+                         new SymbolTableEntry(typedefname, type));
+        return null;
     }
 
     @Override public Void visitModuledef(BSVParser.ModuledefContext ctx) {
-	String modulename = ctx.moduleproto().modulename.getText();
-	String interfacename = "Empty";
-	BSVParser.ModuleformalargsContext moduleformalargs = ctx.moduleproto().moduleformalargs();
-	if (moduleformalargs != null) {
-	    interfacename = moduleformalargs.type(0).getText();
-	}
-	symbolTable.bind(modulename,
-			 new SymbolTableEntry(modulename, interfacename));
-	symbolTable = new SymbolTable(symbolTable);
-	scopes.put(ctx, symbolTable);
-	visitChildren(ctx);
-	symbolTable = symbolTable.parent;
-	return null;
+        String modulename = ctx.moduleproto().modulename.getText();
+        String interfacename = "Empty";
+        BSVParser.ModuleformalargsContext moduleformalargs = ctx.moduleproto().moduleformalargs();
+        if (moduleformalargs != null) {
+            interfacename = moduleformalargs.type(0).getText();
+        }
+        symbolTable.bind(modulename,
+                         new SymbolTableEntry(modulename, interfacename));
+        symbolTable = new SymbolTable(symbolTable);
+        scopes.put(ctx, symbolTable);
+        visitChildren(ctx);
+        symbolTable = symbolTable.parent;
+        return null;
     }
 
     @Override public Void visitRuledef(BSVParser.RuledefContext ruledef) {
-	symbolTable = new SymbolTable(symbolTable);
-	scopes.put(ruledef, symbolTable);
-	visitChildren(ruledef);
-	symbolTable = symbolTable.parent;
-	return null;
+        symbolTable = new SymbolTable(symbolTable);
+        scopes.put(ruledef, symbolTable);
+        visitChildren(ruledef);
+        symbolTable = symbolTable.parent;
+        return null;
     }
 
 
     @Override public Void visitVarBinding(BSVParser.VarBindingContext ctx) {
-	String typeName = ctx.t.getText();
-	for (BSVParser.VarinitContext varinit: ctx.varinit()) {
-	    String varName = varinit.var.getText();
-	    //System.err.println("VarInit " + typeName + " " + varName);
-	    symbolTable.bind(varinit.var.getText(), new SymbolTableEntry(varName, typeName));
-	}
-	return null;
+        String typeName = ctx.t.getText();
+        for (BSVParser.VarinitContext varinit: ctx.varinit()) {
+            String varName = varinit.var.getText();
+            //System.err.println("VarInit " + typeName + " " + varName);
+            symbolTable.bind(varinit.var.getText(), new SymbolTableEntry(varName, typeName));
+        }
+        return null;
     }
 
     @Override public Void visitActionBinding(BSVParser.ActionBindingContext ctx) {
-	String typeName = ctx.t.getText();
-	String varName = ctx.var.getText();
-	//System.err.println("ActionBinding " + typeName + " " + varName);
-	symbolTable.bind(ctx.var.getText(), new SymbolTableEntry(varName, typeName));
-	return null;
+        String typeName = ctx.t.getText();
+        String varName = ctx.var.getText();
+        //System.err.println("ActionBinding " + typeName + " " + varName);
+        symbolTable.bind(ctx.var.getText(), new SymbolTableEntry(varName, typeName));
+        return null;
     }
 
     @Override public Void visitLetBinding(BSVParser.LetBindingContext ctx) {
-	String typeName = "unknown";
-	for (BSVParser.LowerCaseIdentifierContext ident: ctx.lowerCaseIdentifier()) {
-	    String varName = ident.getText();
-	    //System.err.println("VarInit " + typeName + " " + varName);
-	    //Expression rhs = expressionConverter.visit(ctx.rhs);
-	    symbolTable.bind(varName, new SymbolTableEntry(varName, typeName));
-	}
-	return null;
+        String typeName = "unknown";
+        for (BSVParser.LowerCaseIdentifierContext ident: ctx.lowerCaseIdentifier()) {
+            String varName = ident.getText();
+            //System.err.println("VarInit " + typeName + " " + varName);
+            //Expression rhs = expressionConverter.visit(ctx.rhs);
+            symbolTable.bind(varName, new SymbolTableEntry(varName, typeName));
+        }
+        return null;
     }
 }
