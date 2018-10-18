48b5d0ffa81263eb225593c75ab0cb350a978288
Jamey Hicks <jamey.hicks@gmail.com>
2018-10-18 11:19:04 -0400

    placeholder for test cases

---
+++ src/test/java/bsvtokami/MainTest.java
@@ -0,0 +1,11 @@
+package bsvtokami;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+        assertNull(null);
+    }
+}
