840bcb2195945b220dbb48501e3068bb4530563e
Jamey Hicks <jamey.hicks@gmail.com>
2018-10-18 21:45:09 -0400

    implemented pipelined mkProcImpl

--- example/PipelinedProc.bsv
+++ example/PipelinedProc.bsv
@@ -10,6 +10,21 @@ interface ProcRegs;
    method Action write(Bit#(RegFileSz) r, Bit#(DataSz) val);
 endinterface
 
+module mkProcRegs(ProcRegs);
+   RegFile#(Bit#(RegFileSz),Bit#(DataSz)) rf <- mkRegFileFull();
+   method Bit#(DataSz) read1(Bit#(RegFileSz) r1);
+      Bit#(DataSz) val = rf.sub(r1);
+      return val;
+   endmethod
+   method Bit#(DataSz) read2(Bit#(RegFileSz) r2);
+      Bit#(DataSz) val = rf.sub(r2);
+      return val;
+   endmethod
+   method Action write(Bit#(RegFileSz) r, Bit#(DataSz) val);
+      void written <- rf.upd(r, val);
+   endmethod
+endmodule
+
 typedef struct {
    OpK op;
    OpArithK arithOp;
@@ -20,15 +35,11 @@ typedef struct {
    Bit#(PgmSz) pc;
    } D2E deriving (Bits);
 
-interface PipelinedDecoder;
-endinterface
-
-module mkPipelinedDecoder#(Bit#(PgmSz) pcInit,
-			   RegFile#(Bit#(PgmSz), Bit#(InstrSz)) pgm,
-			   Decoder dec,
-			   FIFO#(D2E) d2e)(PipelinedDecoder);
+module mkPipelinedDecoder#(RegFile#(Bit#(PgmSz), Bit#(InstrSz)) pgm,
+                           Decoder dec,
+                           FIFO#(D2E) d2eFifo)(Empty);
    Reg#(Bit#(PgmSz)) pc <- mkReg(0);
-   
+
    rule decode;
       Bit#(InstrSz) inst = pgm.sub(pc);
       OpK op = dec.getOp(inst);
@@ -39,9 +50,9 @@ module mkPipelinedDecoder#(Bit#(PgmSz) pcInit,
       Bit#(AddrSz) addr = dec.getAddr(inst);
 
       D2E decoded = D2E {
-	 op: op, arithOp: arithOp, src1: src1, src2: src2, dst: dst, addr: addr, pc: pc
-	 };
-      void enq <- d2e.enq(decoded);
+         op: op, arithOp: arithOp, src1: src1, src2: src2, dst: dst, addr: addr, pc: pc
+         };
+      void enq <- d2eFifo.enq(decoded);
       pc <= pc + 1;
    endrule
 endmodule
@@ -55,7 +66,7 @@ endinterface
 
 module mkScoreboard(Scoreboard);
    RegFile#(Bit#(RegFileSz), Bool) sbFlags <- mkRegFileFull();
-   
+
    method Bool search1(Bit#(RegFileSz) sidx);
       Bool flag = sbFlags.sub(sidx);
       return flag;
@@ -80,16 +91,18 @@ typedef struct {
    Bit#(DataSz) val;
    } E2W deriving(Bits);
 
-module mkExecuter#(FIFO#(D2E) d2eFifo,
-		   FIFO#(E2W) e2wFifo,
-		   Scoreboard sb,
-		   Executer exec,
-		   ProcRegs rf)(Empty);
+module mkPipelinedExecuter#(FIFO#(D2E) d2eFifo,
+                            FIFO#(E2W) e2wFifo,
+                            Scoreboard sb,
+                            Executer exec,
+                            ProcRegs rf,
+			    Memory mem,
+			    ToHost toHost)(Empty);
    D2E d2e = d2eFifo.first();
    rule executeArith if (d2e.op == opArith
-                         && sb.search1(d2e.src1)
-                         && sb.search2(d2e.src2)
-			);
+                         && !sb.search1(d2e.src1)
+                         && !sb.search2(d2e.src2)
+                        );
       D2E d2e = d2eFifo.first();
       void deq <- d2eFifo.deq();
       Bit#(RegFileSz) src1 = d2e.src1;
@@ -103,17 +116,80 @@ module mkExecuter#(FIFO#(D2E) d2eFifo,
       E2W e2w = E2W { idx: dst, val: execVal };
       void enq <- e2wFifo.enq(e2w);
    endrule
+
+   rule executeLoad if (d2e.op == opLd
+                         && !sb.search1(d2e.src1)
+                         && !sb.search2(d2e.dst)
+                        );
+      D2E d2e = d2eFifo.first();
+      void deq <- d2eFifo.deq();
+      Bit#(RegFileSz) src1 = d2e.src1;
+      Bit#(RegFileSz) dst = d2e.dst;
+      Bit#(AddrSz) addr = d2e.addr;
+      Bit#(DataSz) val1 = rf.read1(src1);
+      MemRq memrq = MemRq { isLoad: 1'b1, addr: addr, data: 32'b0 };
+      Bit#(DataSz) ldVal <- mem.doMem(memrq);
+      void insert <- sb.insert(dst);
+      E2W e2w = E2W { idx: dst, val: ldVal };
+      void enq <- e2wFifo.enq(e2w);
+   endrule
+
+   rule executeStore if (d2e.op == opSt
+                         && !sb.search1(d2e.src1)
+                        );
+      D2E d2e = d2eFifo.first();
+      void deq <- d2eFifo.deq();
+      Bit#(RegFileSz) src1 = d2e.src1;
+      Bit#(AddrSz) addr = d2e.addr;
+      Bit#(DataSz) val1 = rf.read1(src1);
+      MemRq memrq = MemRq { isLoad: 1'b0, addr: addr, data: val1 };
+      Bit#(DataSz) unused <- mem.doMem(memrq);
+   endrule
+
+   rule executeToHost if (d2e.op == opTh
+                         && !sb.search1(d2e.src1)
+                        );
+      D2E d2e = d2eFifo.first();
+      void deq <- d2eFifo.deq();
+      Bit#(RegFileSz) src1 = d2e.src1;
+      Bit#(AddrSz) addr = d2e.addr;
+      Bit#(DataSz) val1 = rf.read1(src1);
+      void unused <- toHost.toHost(val1);
+   endrule
+
 endmodule
 
-module mkWriteBack#(FIFO#(E2W) e2wFifo,
-		    Scoreboard sb,
-		    ProcRegs rf)(Empty);
+module mkPipelinedWriteback#(FIFO#(E2W) e2wFifo,
+                             Scoreboard sb,
+                             ProcRegs rf)(Empty);
    rule writeback;
       E2W e2w = e2wFifo.first();
       void deq <- e2wFifo.deq();
       Bit#(RegFileSz) idx = e2w.idx;
       Bit#(DataSz) val = e2w.val;
-      void written <- rf.write(idx, val); 
+      void written <- rf.write(idx, val);
       void removed <- sb.remove(idx);
    endrule
+endmodule
+
+module mkProcImpl#(RegFile#(Bit#(PgmSz), Bit#(InstrSz)) pgm,
+                   Decoder dec,
+                   Executer exec,
+		   ToHost toHost)(Empty);
+   FIFO#(D2E) d2eFifo <- mkFIFO();
+   FIFO#(E2W) e2wFifo <- mkFIFO();
+   Memory mem <- mkMemory();
+   ProcRegs rf <- mkProcRegs();
+   Scoreboard sb <- mkScoreboard();
+   Empty decoder <- mkPipelinedDecoder(pgm, dec, d2eFifo);
+   Empty executer <- mkPipelinedExecuter(d2eFifo,
+                                         e2wFifo,
+                                         sb,
+                                         exec,
+                                         rf,
+					 mem,
+					 toHost);
+   Empty writeback <- mkPipelinedWriteback(e2wFifo,
+                                           sb,
+                                           rf);
 endmodule
--- example/out/PipelinedProc.v
+++ example/out/PipelinedProc.v
@@ -27,6 +27,44 @@ Hint Unfold ProcRegs'read1 : ModuleDefs.
 Hint Unfold ProcRegs'read2 : ModuleDefs.
 Hint Unfold ProcRegs'write : ModuleDefs.
 
+Module module'mkProcRegs.
+    Section Section'mkProcRegs.
+    Variable instancePrefix: string.
+        (* method bindings *)
+    (* method binding *) Let rf := mkRegFileFull (Bit RegFileSz) (Bit DataSz) (instancePrefix--"rf").
+    (* instance methods *)
+    Let rfsub : string := (RegFile'sub rf).
+    Let rfupd : string := (RegFile'upd rf).
+    Definition mkProcRegsModule: Modules :=
+         (BKMODULE {
+        (BKMod (RegFile'modules rf :: nil))
+    with Method instancePrefix--"read1" (r1 : (Bit RegFileSz)) : Bit DataSz :=
+    (
+CallM val : Bit DataSz (* varbinding *) <-  rfsub (#r1 : Bit RegFileSz);
+        Ret #val    )
+
+    with Method instancePrefix--"read2" (r2 : (Bit RegFileSz)) : Bit DataSz :=
+    (
+CallM val : Bit DataSz (* varbinding *) <-  rfsub (#r2 : Bit RegFileSz);
+        Ret #val    )
+
+    with Method instancePrefix--"write" (r : (Bit RegFileSz)) (val : (Bit DataSz)) : Void :=
+    (
+        CallM written : Void (* actionBinding *) <- rfupd (#r : Bit RegFileSz) (#val : Bit DataSz);
+        Retv    )
+
+    }). (* mkProcRegs *)
+
+(* Module mkProcRegs type Module#(ProcRegs) return type ProcRegs *)
+    Definition mkProcRegs := Build_ProcRegs mkProcRegsModule%kami (instancePrefix--"read1") (instancePrefix--"read2") (instancePrefix--"write").
+    End Section'mkProcRegs.
+End module'mkProcRegs.
+
+Definition mkProcRegs := module'mkProcRegs.mkProcRegs.
+Hint Unfold mkProcRegs : ModuleDefs.
+Hint Unfold module'mkProcRegs.mkProcRegs : ModuleDefs.
+Hint Unfold module'mkProcRegs.mkProcRegsModule : ModuleDefs.
+
 Definition D2EFields := (STRUCT {
     "addr" :: Bit AddrSz;
     "arithOp" :: OpArithK;
@@ -37,26 +75,18 @@ Definition D2EFields := (STRUCT {
     "src2" :: Bit RegFileSz}).
 Definition D2E  := Struct (D2EFields).
 
-(* * interface PipelinedDecoder *)
-Record PipelinedDecoder := {
-    PipelinedDecoder'modules: Modules;
-}.
-
-Hint Unfold PipelinedDecoder'modules : ModuleDefs.
-
 Module module'mkPipelinedDecoder.
     Section Section'mkPipelinedDecoder.
     Variable instancePrefix: string.
-    Variable pcInit: ConstT (Bit PgmSz).
     Variable pgm: RegFile.
     Variable dec: Decoder.
-    Variable d2e: FIFO.
+    Variable d2eFifo: FIFO.
         (* method bindings *)
     (* method binding *) Let pc := mkReg (Bit PgmSz) (instancePrefix--"pc") ($0)%bk.
     (* method binding *) Let pc_read : string := (Reg'_read pc).
     (* method binding *) Let pc_write : string := (Reg'_write pc).
     (* instance methods *)
-    Let d2eenq : string := (FIFO'enq d2e).
+    Let d2eFifoenq : string := (FIFO'enq d2eFifo).
     Let decgetAddr : string := (Decoder'getAddr dec).
     Let decgetArithOp : string := (Decoder'getArithOp dec).
     Let decgetDst : string := (Decoder'getDst dec).
@@ -78,13 +108,13 @@ Module module'mkPipelinedDecoder.
        CallM dst : Bit RegFileSz (* varbinding *) <-  decgetDst (#inst : Bit InstrSz);
        CallM addr : Bit AddrSz (* varbinding *) <-  decgetAddr (#inst : Bit InstrSz);
                LET decoded : D2E <- STRUCT { "addr" ::= (#addr); "arithOp" ::= (#arithOp); "dst" ::= (#dst); "op" ::= (#op); "pc" ::= (#pc_v); "src1" ::= (#src1); "src2" ::= (#src2)  }%kami_expr;
-       CallM call5 : Void <-  d2eenq (#decoded : D2E);
+               CallM enq : Void (* actionBinding *) <- d2eFifoenq (#decoded : D2E);
                CallM pc_write ( (#pc_v + $1) : Bit PgmSz );
         Retv ) (* rule decode *)
     }). (* mkPipelinedDecoder *)
 
-(* Module mkPipelinedDecoder type Bit#(PgmSz) -> RegFile#(Bit#(PgmSz), Bit#(InstrSz)) -> Decoder -> FIFO#(D2E) -> Module#(PipelinedDecoder) return type RegFile#(Bit#(PgmSz), Bit#(InstrSz)) *)
-    Definition mkPipelinedDecoder := Build_PipelinedDecoder mkPipelinedDecoderModule%kami.
+(* Module mkPipelinedDecoder type RegFile#(Bit#(PgmSz), Bit#(InstrSz)) -> Decoder -> FIFO#(D2E) -> Module#(Empty) return type Decoder *)
+    Definition mkPipelinedDecoder := Build_Empty mkPipelinedDecoderModule%kami.
     End Section'mkPipelinedDecoder.
 End module'mkPipelinedDecoder.
 
@@ -112,7 +142,7 @@ Module module'mkScoreboard.
     Section Section'mkScoreboard.
     Variable instancePrefix: string.
         (* method bindings *)
-    (* method binding *) Let sbFlags := mkRegFileFull (Bit RegFileSz) (Bit DataSz) (instancePrefix--"sbFlags").
+    (* method binding *) Let sbFlags := mkRegFileFull (Bit RegFileSz) Bool (instancePrefix--"sbFlags").
     (* instance methods *)
     Let sbFlagssub : string := (RegFile'sub sbFlags).
     Let sbFlagsupd : string := (RegFile'upd sbFlags).
@@ -156,32 +186,36 @@ Definition E2WFields := (STRUCT {
     "val" :: Bit DataSz}).
 Definition E2W  := Struct (E2WFields).
 
-Module module'mkExecuter.
-    Section Section'mkExecuter.
+Module module'mkPipelinedExecuter.
+    Section Section'mkPipelinedExecuter.
     Variable instancePrefix: string.
     Variable d2eFifo: FIFO.
     Variable e2wFifo: FIFO.
     Variable sb: Scoreboard.
     Variable exec: Executer.
     Variable rf: ProcRegs.
+    Variable mem: Memory.
+    Variable toHost: ToHost.
     (* instance methods *)
     Let d2eFifodeq : string := (FIFO'deq d2eFifo).
     Let d2eFifofirst : string := (FIFO'first d2eFifo).
     Let e2wFifoenq : string := (FIFO'enq e2wFifo).
     Let execexecArith : string := (Executer'execArith exec).
+    Let memdoMem : string := (Memory'doMem mem).
     Let rfread1 : string := (ProcRegs'read1 rf).
     Let sbinsert : string := (Scoreboard'insert sb).
     Let sbsearch1 : string := (Scoreboard'search1 sb).
     Let sbsearch2 : string := (Scoreboard'search2 sb).
-    Definition mkExecuterModule: Modules :=
+    Let toHosttoHost : string := (ToHost'toHost toHost).
+    Definition mkPipelinedExecuterModule: Modules :=
          (BKMODULE {
         Rule instancePrefix--"executeArith" :=
     (
        CallM d2e : D2E (* varbinding *) <-  d2eFifofirst ();
-       CallM call6 : Bool <-  sbsearch1 ((#d2e ! D2EFields @. "src1") : Bit RegFileSz);
-       CallM call7 : Bool <-  sbsearch2 ((#d2e ! D2EFields @. "src2") : Bit RegFileSz);
+       CallM call5 : Bool <-  sbsearch1 ((#d2e ! D2EFields @. "src1") : Bit RegFileSz);
+       CallM call6 : Bool <-  sbsearch2 ((#d2e ! D2EFields @. "src2") : Bit RegFileSz);
 
-        Assert(((((#d2e ! D2EFields @. "op") == $$opArith) && #call6) && #call7));
+        Assert(((((#d2e ! D2EFields @. "op") == $$opArith) && (!#call5)) && (!#call6)));
                CallM deq : Void (* actionBinding *) <- d2eFifodeq ();
                LET src1 : Bit RegFileSz <- (#d2e ! D2EFields @. "src1");
                LET src2 : Bit RegFileSz <- (#d2e ! D2EFields @. "src2");
@@ -194,20 +228,63 @@ Module module'mkExecuter.
                LET e2w : E2W <- STRUCT { "idx" ::= (#dst); "val" ::= (#execVal)  }%kami_expr;
                CallM enq : Void (* actionBinding *) <- e2wFifoenq (#e2w : E2W);
         Retv ) (* rule executeArith *)
-    }). (* mkExecuter *)
+    with Rule instancePrefix--"executeLoad" :=
+    (
+       CallM d2e : D2E (* varbinding *) <-  d2eFifofirst ();
+       CallM call7 : Bool <-  sbsearch1 ((#d2e ! D2EFields @. "src1") : Bit RegFileSz);
+       CallM call8 : Bool <-  sbsearch2 ((#d2e ! D2EFields @. "dst") : Bit RegFileSz);
 
-(* Module mkExecuter type FIFO#(D2E) -> FIFO#(E2W) -> Scoreboard -> Executer -> ProcRegs -> Module#(Empty) return type FIFO#(E2W) *)
-    Definition mkExecuter := Build_Empty mkExecuterModule%kami.
-    End Section'mkExecuter.
-End module'mkExecuter.
+        Assert(((((#d2e ! D2EFields @. "op") == $$opLd) && (!#call7)) && (!#call8)));
+               CallM deq : Void (* actionBinding *) <- d2eFifodeq ();
+               LET src1 : Bit RegFileSz <- (#d2e ! D2EFields @. "src1");
+               LET dst : Bit RegFileSz <- (#d2e ! D2EFields @. "dst");
+               LET addr : Bit AddrSz <- (#d2e ! D2EFields @. "addr");
+       CallM val1 : Bit DataSz (* varbinding *) <-  rfread1 (#src1 : Bit RegFileSz);
+               LET memrq : MemRq <- STRUCT { "addr" ::= (#addr); "data" ::= ($$(natToWord 32 0)); "isLoad" ::= ($$(natToWord 1 1))  }%kami_expr;
+               CallM ldVal : Bit DataSz (* actionBinding *) <- memdoMem (#memrq : MemRq);
+               CallM insert : Void (* actionBinding *) <- sbinsert (#dst : Bit RegFileSz);
+               LET e2w : E2W <- STRUCT { "idx" ::= (#dst); "val" ::= (#ldVal)  }%kami_expr;
+               CallM enq : Void (* actionBinding *) <- e2wFifoenq (#e2w : E2W);
+        Retv ) (* rule executeLoad *)
+    with Rule instancePrefix--"executeStore" :=
+    (
+       CallM d2e : D2E (* varbinding *) <-  d2eFifofirst ();
+       CallM call9 : Bool <-  sbsearch1 ((#d2e ! D2EFields @. "src1") : Bit RegFileSz);
 
-Definition mkExecuter := module'mkExecuter.mkExecuter.
-Hint Unfold mkExecuter : ModuleDefs.
-Hint Unfold module'mkExecuter.mkExecuter : ModuleDefs.
-Hint Unfold module'mkExecuter.mkExecuterModule : ModuleDefs.
+        Assert((((#d2e ! D2EFields @. "op") == $$opSt) && (!#call9)));
+               CallM deq : Void (* actionBinding *) <- d2eFifodeq ();
+               LET src1 : Bit RegFileSz <- (#d2e ! D2EFields @. "src1");
+               LET addr : Bit AddrSz <- (#d2e ! D2EFields @. "addr");
+       CallM val1 : Bit DataSz (* varbinding *) <-  rfread1 (#src1 : Bit RegFileSz);
+               LET memrq : MemRq <- STRUCT { "addr" ::= (#addr); "data" ::= (#val1); "isLoad" ::= ($$(natToWord 1 0))  }%kami_expr;
+               CallM unused : Bit DataSz (* actionBinding *) <- memdoMem (#memrq : MemRq);
+        Retv ) (* rule executeStore *)
+    with Rule instancePrefix--"executeToHost" :=
+    (
+       CallM d2e : D2E (* varbinding *) <-  d2eFifofirst ();
+       CallM call10 : Bool <-  sbsearch1 ((#d2e ! D2EFields @. "src1") : Bit RegFileSz);
 
-Module module'mkWriteBack.
-    Section Section'mkWriteBack.
+        Assert((((#d2e ! D2EFields @. "op") == $$opTh) && (!#call10)));
+               CallM deq : Void (* actionBinding *) <- d2eFifodeq ();
+               LET src1 : Bit RegFileSz <- (#d2e ! D2EFields @. "src1");
+               LET addr : Bit AddrSz <- (#d2e ! D2EFields @. "addr");
+       CallM val1 : Bit DataSz (* varbinding *) <-  rfread1 (#src1 : Bit RegFileSz);
+               CallM unused : Void (* actionBinding *) <- toHosttoHost (#val1 : Bit DataSz);
+        Retv ) (* rule executeToHost *)
+    }). (* mkPipelinedExecuter *)
+
+(* Module mkPipelinedExecuter type FIFO#(D2E) -> FIFO#(E2W) -> Scoreboard -> Executer -> ProcRegs -> Memory -> ToHost -> Module#(Empty) return type FIFO#(E2W) *)
+    Definition mkPipelinedExecuter := Build_Empty mkPipelinedExecuterModule%kami.
+    End Section'mkPipelinedExecuter.
+End module'mkPipelinedExecuter.
+
+Definition mkPipelinedExecuter := module'mkPipelinedExecuter.mkPipelinedExecuter.
+Hint Unfold mkPipelinedExecuter : ModuleDefs.
+Hint Unfold module'mkPipelinedExecuter.mkPipelinedExecuter : ModuleDefs.
+Hint Unfold module'mkPipelinedExecuter.mkPipelinedExecuterModule : ModuleDefs.
+
+Module module'mkPipelinedWriteback.
+    Section Section'mkPipelinedWriteback.
     Variable instancePrefix: string.
     Variable e2wFifo: FIFO.
     Variable sb: Scoreboard.
@@ -217,7 +294,7 @@ Module module'mkWriteBack.
     Let e2wFifofirst : string := (FIFO'first e2wFifo).
     Let rfwrite : string := (ProcRegs'write rf).
     Let sbremove : string := (Scoreboard'remove sb).
-    Definition mkWriteBackModule: Modules :=
+    Definition mkPipelinedWritebackModule: Modules :=
          (BKMODULE {
         Rule instancePrefix--"writeback" :=
     (
@@ -228,15 +305,53 @@ Module module'mkWriteBack.
                CallM written : Void (* actionBinding *) <- rfwrite (#idx : Bit RegFileSz) (#val : Bit DataSz);
                CallM removed : Void (* actionBinding *) <- sbremove (#idx : Bit RegFileSz);
         Retv ) (* rule writeback *)
-    }). (* mkWriteBack *)
+    }). (* mkPipelinedWriteback *)
 
-(* Module mkWriteBack type FIFO#(E2W) -> Scoreboard -> ProcRegs -> Module#(Empty) return type Scoreboard *)
-    Definition mkWriteBack := Build_Empty mkWriteBackModule%kami.
-    End Section'mkWriteBack.
-End module'mkWriteBack.
+(* Module mkPipelinedWriteback type FIFO#(E2W) -> Scoreboard -> ProcRegs -> Module#(Empty) return type Scoreboard *)
+    Definition mkPipelinedWriteback := Build_Empty mkPipelinedWritebackModule%kami.
+    End Section'mkPipelinedWriteback.
+End module'mkPipelinedWriteback.
 
-Definition mkWriteBack := module'mkWriteBack.mkWriteBack.
-Hint Unfold mkWriteBack : ModuleDefs.
-Hint Unfold module'mkWriteBack.mkWriteBack : ModuleDefs.
-Hint Unfold module'mkWriteBack.mkWriteBackModule : ModuleDefs.
+Definition mkPipelinedWriteback := module'mkPipelinedWriteback.mkPipelinedWriteback.
+Hint Unfold mkPipelinedWriteback : ModuleDefs.
+Hint Unfold module'mkPipelinedWriteback.mkPipelinedWriteback : ModuleDefs.
+Hint Unfold module'mkPipelinedWriteback.mkPipelinedWritebackModule : ModuleDefs.
+
+Module module'mkProcImpl.
+    Section Section'mkProcImpl.
+    Variable instancePrefix: string.
+    Variable pgm: RegFile.
+    Variable dec: Decoder.
+    Variable exec: Executer.
+    Variable toHost: ToHost.
+        (* method bindings *)
+    (* method binding *) Let d2eFifo := mkFIFO D2E (instancePrefix--"d2eFifo").
+    (* method binding *) Let e2wFifo := mkFIFO E2W (instancePrefix--"e2wFifo").
+    (* method binding *) Let mem := mkMemory (instancePrefix--"mem").
+    (* method binding *) Let rf := mkProcRegs (instancePrefix--"rf").
+    (* method binding *) Let sb := mkScoreboard (instancePrefix--"sb").
+    (* method binding *) Let decoder := mkPipelinedDecoder (instancePrefix--"decoder") (pgm)%bk (dec)%bk (d2eFifo)%bk.
+    (* method binding *) Let executer := mkPipelinedExecuter (instancePrefix--"executer") (d2eFifo)%bk (e2wFifo)%bk (sb)%bk (exec)%bk (rf)%bk (mem)%bk (toHost)%bk.
+    (* method binding *) Let writeback := mkPipelinedWriteback (instancePrefix--"writeback") (e2wFifo)%bk (sb)%bk (rf)%bk.
+    Definition mkProcImplModule: Modules :=
+         (BKMODULE {
+        (BKMod (FIFO'modules d2eFifo :: nil))
+    with (BKMod (FIFO'modules e2wFifo :: nil))
+    with (BKMod (Memory'modules mem :: nil))
+    with (BKMod (ProcRegs'modules rf :: nil))
+    with (BKMod (Scoreboard'modules sb :: nil))
+    with (BKMod (Empty'modules decoder :: nil))
+    with (BKMod (Empty'modules executer :: nil))
+    with (BKMod (Empty'modules writeback :: nil))
+    }). (* mkProcImpl *)
+
+(* Module mkProcImpl type RegFile#(Bit#(PgmSz), Bit#(InstrSz)) -> Decoder -> Executer -> ToHost -> Module#(Empty) return type Decoder *)
+    Definition mkProcImpl := Build_Empty mkProcImplModule%kami.
+    End Section'mkProcImpl.
+End module'mkProcImpl.
+
+Definition mkProcImpl := module'mkProcImpl.mkProcImpl.
+Hint Unfold mkProcImpl : ModuleDefs.
+Hint Unfold module'mkProcImpl.mkProcImpl : ModuleDefs.
+Hint Unfold module'mkProcImpl.mkProcImplModule : ModuleDefs.
 
